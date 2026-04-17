5d5562cc8b61d790251215f830f29948345d36e9
Simon Masserey <simasserey@gmail.com>
2026-04-17 11:40:04 +0200

    fix: database seeding polish

--- MVP/database_model/seed.py
+++ MVP/database_model/seed.py
@@ -10,14 +10,6 @@ def seed():
     doctors = [
         Doctor(
             telegram_id=int(os.getenv("DOCTOR_1_TELEGRAM_ID")),
-            name="Gregory", surname="House"
-        ),
-        Doctor(
-            telegram_id=int(os.getenv("DOCTOR_2_TELEGRAM_ID")),
-            name="Meredith", surname="Grey"
-        ),
-        Doctor(
-            telegram_id=int(os.getenv("DOCTOR_3_TELEGRAM_ID")),
             name="Robin", surname="Bütikofer"
         ),
     ]
@@ -29,10 +21,7 @@ def seed():
             session.refresh(doctor)
 
         patients = [
-            Patient(telegram_id=int(os.getenv("PATIENT_1_TELEGRAM_ID")), name="John", surname="Doe", doctor_id=doctors[0].id),
-            Patient(telegram_id=int(os.getenv("PATIENT_2_TELEGRAM_ID")), name="Jane", surname="Smith", doctor_id=doctors[0].id),
-            Patient(telegram_id=int(os.getenv("PATIENT_3_TELEGRAM_ID")), name="Arthur", surname="Morgan", doctor_id=doctors[1].id),
-            Patient(telegram_id=int(os.getenv("PATIENT_4_TELEGRAM_ID")), name="Simon", surname="Masserey", doctor_id=doctors[2].id),
+            Patient(telegram_id=int(os.getenv("PATIENT_1_TELEGRAM_ID")), name="Simon", surname="Masserey", doctor_id=doctors[0].id),
         ]
 
         session.add_all(patients)
